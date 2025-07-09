ca7a4651d6f216ef553201bd5fbaff59786282b8
Javier Quiroga <contact@javierquiroga.com>
2025-07-09 11:06:32 +0100

    feat: Create CV Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import './App.css';
+import CVRender from './CVRender';
+
+const cvStructure = {
+  info: {
+    name: 'Javier Quiroga',
+    phone: '07413 174457',
+    email: 'contact@javierquiroga.com',
+    website: 'www.javierquiroga.com',
+  },
+  education: [
+    {
+      school: 'Uni',
+      title: 'UX Design',
+      date: '20-20-20',
+      details: '-Blablabla, Blablabla blablabla',
+    },
+  ],
+  experience: [
+    {
+      company: 'OS',
+      position: 'GM',
+      dateFrom: '20-20',
+      dateTo: '20-21',
+      details: '- Blobloblo blobloblo',
+    },
+  ],
+  skills: ['one', 'two', 'three'],
+};
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <CVRender cv={cvStructure} />
+    </>
+  );
 }
 
 export default App;
